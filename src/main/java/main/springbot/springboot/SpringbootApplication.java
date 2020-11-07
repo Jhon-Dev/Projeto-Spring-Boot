6404cf815a421d2daf06f55cf16d5fdd031f8c5f
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-07 03:48:45 -0300

    Configurando JPA + bando de dados

--- src/main/java/main/springbot/springboot/SpringbootApplication.java
+++ src/main/java/main/springbot/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package main.springbot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages="main.springboot.model")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
