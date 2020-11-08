585f168dda83760c1ca9d8e568e5cdb42ce7f504
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-08 09:06:22 -0300

    Criando um filtro pelo nome das pessoas

--- src/main/java/main/springboot/controller/PessoaController.java
+++ src/main/java/main/springboot/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import main.springboot.model.Pessoa;
@@ -68,6 +70,15 @@ public class PessoaController {
 		modelAndView.addObject("pessoas", pessoaRepository.findAll());
 		modelAndView.addObject("pessoaobj", new Pessoa());
 
+		return modelAndView;
+	}
+	
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findPessoasByName(nomepesquisa));
+		modelAndView.addObject("pessoaobj", new Pessoa());
 		return modelAndView;
 	}
 
--- src/main/java/main/springboot/repository/PessoaRepository.java
+++ src/main/java/main/springboot/repository/PessoaRepository.java
@@ -1,7 +1,10 @@
 package main.springboot.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,6 @@ import main.springboot.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoasByName(String name);
 }
