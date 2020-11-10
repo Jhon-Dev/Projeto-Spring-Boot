590fc7f04931363c988826181eef3d6432d347d5
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-10 00:07:34 -0300

    Realizando validações com Spring

--- src/main/java/main/springboot/controller/PessoaController.java
+++ src/main/java/main/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package main.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +43,26 @@ public class PessoaController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
-	public ModelAndView salvar(Pessoa pessoa) {
+	
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			modelAndView.addObject("pessoas", pessoasIt);
+			modelAndView.addObject("pessoaobj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			for(ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage()); // vem das Anotações @NotEmpty e outras
+			}
+			
+			modelAndView.addObject("msg", msg);
+			
+			return modelAndView;			
+			
+		}
+				
 		pessoaRepository.save(pessoa);
 		
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
@@ -45,6 +70,7 @@ public class PessoaController {
 		modelAndView.addObject("pessoas", pessoasIt);
 		modelAndView.addObject("pessoaobj", new Pessoa());
 		
+
 		return modelAndView;
 	}
 	
@@ -114,9 +140,6 @@ public class PessoaController {
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		modelAndView.addObject("pessoaobj", pessoa);
 		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
-
-		
-
 
 		return modelAndView;
 	}
--- src/main/java/main/springboot/model/Pessoa.java
+++ src/main/java/main/springboot/model/Pessoa.java
@@ -9,6 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pessoa  implements Serializable{
@@ -22,10 +26,17 @@ public class Pessoa  implements Serializable{
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@NotNull(message = "Nome não pode ser nulo")
+	@NotEmpty(message = "Nome não pode ser vazio")
+	@NotBlank(message = "Nome não pode ser vazio")
 	private String nome;
 	
+	@NotNull(message = "Sobrenome não pode ser nulo")
+	@NotEmpty(message = "Sobrenome não pode ser vazio")
+	@NotBlank(message = "Sobrenome não pode ser vazio")
 	private String sobrenome;
 	
+	@Min(value = 18, message = "Idade inválida")
 	private int idade;
 	
 	
