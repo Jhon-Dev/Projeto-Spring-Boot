76c103dc6ca99889610dd8e7208b926553c64862
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-13 18:37:21 -0300

    Sprinf + JPA + Banco de dados - Validando

---
+++ src/main/java/main/springboot/security/ImplementacaoUserDetailsService.java
@@ -0,0 +1,30 @@
+package main.springboot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import main.springboot.model.Usuario;
+import main.springboot.repository.UsuarioRepository;
+
+@Service
+public class ImplementacaoUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+		
+		if (usuario == null) {
+			throw new UsernameNotFoundException("Usuário não foi encontrado");
+		}
+		
+		return usuario;
+	}
+
+}
--- src/main/java/main/springboot/security/WebConfigSecurity.java
+++ src/main/java/main/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package main.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,6 +16,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
 	@Override  // Configura as solicitações de acesso por Http
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
@@ -29,10 +34,14 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	@Override // Cria autenticação do usuário com o banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+	/*auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.withUser("Jhonatan")
-		.password("123")
-		.roles("ADMIN");
+		.password("$2a$10$HurEblbEeN.sWhsTM/grIuWRH9VJekXughPG4E2aqrN71HI3bYQbe")
+		.roles("ADMIN");*/
+		
 	}
 	
 	@Override // Ignora URL especificas
